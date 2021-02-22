a882e2f7baed4af3f214409ab3ea3ed2a6ea6fb2
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-02-22 18:51:55 +0100

    refactor: navbar (header)
    Add dropdown links with more sublinks.
    Change phone icon and font size of call links.

--- routes.php
+++ routes.php
@@ -1,3 +1,4 @@
 <?php
 
 $router->get('', 'views/index.view.php');
+$router->get('opsta', 'views/opsta.view.php');
--- views/partials/header.php
+++ views/partials/header.php
@@ -14,7 +14,7 @@
 
 	<!-- Styles -->
 	<script src="https://use.fontawesome.com/releases/v5.15.2/js/all.js"></script>
-	<link href="style.css" rel="stylesheet">
+	<link href="views/partials/style.css" rel="stylesheet">
 
 	<!-- Fav Icon -->
 	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
@@ -27,28 +27,64 @@
 </head>
 <body>
 	<!-- Navigation -->
-	<nav class="navbar navbar-expand-lg navbar-light sticky-top p-0">
-		<div class="container-fluid" style="background: #9fdfbf">
-			<a class="navbar-brand" href="/"><img class="logo" src="images/logo.png"></a>
+	<nav class="navbar navbar-expand-xl navbar-light sticky-top p-0" style="background: #9fdfbf">
+		<div class="container-fluid pr-3">
+			<a class="navbar-brand" href="/"><img class="logo pl-3" src="images/logo.png"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="opsta">Opšta Stomatologija</a>
+				<ul class="navbar-nav ml-auto pl-3">
+					<li class="nav-item dropdown active">
+						<a class="nav-link dropdown-toggle" href="opsta" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Opšta Stomatologija
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Lečenje Zuba</a>
+							<a class="dropdown-item" href="#">Paradontologija</a>
+							<a class="dropdown-item" href="#">Dečija Stomatologija</a>
+							<a class="dropdown-item" href="#">Restauracija Zuba (Plombe)</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown active">
+						<a class="nav-link dropdown-toggle" href="opsta" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Estetska Stomatologija
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Ortodoncija</a>
+							<a class="dropdown-item" href="#">Viniri (Fasete)</a>
+							<a class="dropdown-item" href="#">Izbeljivanje Zuba</a>
+							<a class="dropdown-item" href="#">Bezmetalna Keramika</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown active">
+						<a class="nav-link dropdown-toggle" href="opsta" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Oralna Hirurgija
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Vađenje Zuba</a>
+							<a class="dropdown-item" href="#">Vađenje Umnjaka</a>
+							<a class="dropdown-item" href="#">Ostali Hirurški Zahvati</a>
+							<a class="dropdown-item" href="#">Hirurško Vađenje Zuba</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown active">
+						<a class="nav-link dropdown-toggle" href="opsta" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Protetika
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Krunice</a>
+							<a class="dropdown-item" href="#">Proteze</a>
+							<a class="dropdown-item" href="#">Mostovi</a>
+							<a class="dropdown-item" href="#">Ortodoncija</a>
+							<a class="dropdown-item" href="#">Terapeutski Splintovi</a>
+						</div>
 					</li>
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Estetska Stomatologija</a>
+						<a class="nav-link" href="#">Zašto Mi?</a>
 					</li>
 					<li class="nav-item active">
-						<a class="nav-link" href="#">Oralna Hirurgija</a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Protetika</a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Ortodoncija</a>
+						<a class="nav-link" href="#">Pitanja i Odgovori</a>
 					</li>
 				</ul>
 			</div>
@@ -78,7 +114,7 @@
 			</div>
 			<div class="col-6 col-sm-6 col-md-3">
 				<a href="tel:0113189713">
-					<i class="fas fa-phone-alt"></i>
+					<i class="fas fa-phone-square"></i>
 					<p>011 3189 713</p>
 				</a>
 			</div>
