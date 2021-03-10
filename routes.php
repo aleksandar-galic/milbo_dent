5832778766494f2926a13389045052d939abc496
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-03-10 15:39:11 +0100

    feat: estetska stomatologija

--- routes.php
+++ routes.php
@@ -10,5 +10,8 @@ $router->get('lecenje-zuba', 'views/lecenje_zuba.view.php');
 $router->get('paradontologija', 'views/paradontologija.view.php');
 $router->get('decija-stomatologija', 'views/decija_stomatologija.view.php');
 
+// Estetska Stomatologija
+$router->get('estetska-stomatologija', 'views/estetska-stomatologija.view.php');
+
 // Ostalo
 $router->get('pitanja-i-odgovori', 'views/pitanja_i_odgovori.view.php');
---
+++ views/estetska-stomatologija.view.php
@@ -0,0 +1,120 @@
+<?php require ('partials/header.php'); ?>
+
+	<!-- About Section -->
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Estetska Stomatologija</h1>
+			</div>
+			<hr class="my-3">
+			<div class="col-12">
+				<p class="lead">
+					Živimo u vremenu kada je estetika zauzela presudnu ulogu u životima ljudi. Brojna istraživanja su pokazala da ljudi na licu osobe koju posmatraju prvo primete zube i osmeh.
+					<br><br>
+					Dolaskom u našu ordinaciju dovedite svoj osmeh do perfekcije. Pružite prijatno zadovoljstvo sebi i drugima kada se osmehujete.
+				</p>
+			</div>
+		</div>
+		<hr class="my-3">
+	</div>
+
+	<!-- Cards -->
+	<div class="container-xl">
+		<div class="d-flex row justify-content-around">
+			<div class="card m-4 shadow" style="width: 20rem; height: auto;">
+				<img src="images/Estetika/4.jpg" class="card-img-top" alt="..." style="height: 8rem;">
+				<div class="card-body">
+					<h4 class="card-title">Ortodoncija</h4>
+					<hr class="mt-0">
+					<p class="card-text">
+						Ortodoncija je grana stomatologije koja se bavi korekcijom nepravilnosti vilica i zuba.
+						<br><br>
+						Nepravilnosti se uocavaju vec kod dece školskog uzrasta. U zavisnosti od nepravilnosti sa terapijom se zapocinje što pre...
+					</p>
+					<a href="ortodoncija" class="btn btn-primary">Pogledaj</a>
+				</div>
+			</div>
+			<div class="card m-4 shadow" style="width: 20rem; height: auto;">
+				<img src="images/Estetika/3.jpg" class="card-img-top" alt="..." style="height: 8rem;">
+				<div class="card-body">
+					<h4 class="card-title">Viniri (Fasete)</h4>
+					<hr class="mt-0">
+					<p class="card-text">
+						Viniri predstavljaju keramicke fasete ili ljuspice, kako ih još nazivaju.
+						<br><br>
+						Radenjem vinira vi ste u stanju da potpuno rekonstruišete svoj osmeh: od boje zuba, preko ispravljanja pomerenih, rotiranih...
+					</p>
+					<a href="viniri" class="btn btn-primary">Pogledaj</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="d-flex row justify-content-around">
+			<div class="card m-4 shadow" style="width: 20rem; height: auto;">
+				<img src="images/Estetika/1.jpg" class="card-img-top" alt="..." style="height: 8rem;">
+				<div class="card-body">
+					<h4 class="card-title">Izbeljivanje Zuba</h4>
+					<hr class="mt-0">
+					<p class="card-text">
+						Estetika u stomatologiji je veoma povezana sa bojom vaših zuba.
+						<br><br>
+						Beljenjem zuba u našoj ordinaciji dobijate holivudski osmeh kojim se svako ponosi.
+						<br><br>
+						Pružamo dve vrste beljenja: ambulantno sa laserom i kucnu varijantu sa trejom...
+					</p>
+					<a href="beljenje-zuba" class="btn btn-primary">Pogledaj</a>
+				</div>
+			</div>
+			<div class="card m-4 shadow" style="width: 20rem; height: auto;">
+				<img src="images/Estetika/2.jpg" class="card-img-top" alt="..." style="height: 8rem;">
+				<div class="card-body">
+					<h4 class="card-title">Bezmetalna Keramika</h4>
+					<hr class="mt-0">
+					<p class="card-text">
+						Razvojem stomatologije danas se došlo do najidealnijeg nacina za rekonstrukciju vaših zuba, narocito u frontalnoj regiji.
+						<br><br>
+						Krunice i mostovi radeni od bezmetalne keramike su pravo estetsko savršenstvo u pogledu boje, transparencije i translucencije zuba...
+					</p>
+					<a href="bezmetalna-keramika" class="btn btn-primary">Pogledaj</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Cenovnik -->
+	<div class="container">
+		<h2>Cenovnik</h2>
+		<table class="table table-bordered">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Usluga</th>
+					<th scope="col">Cena</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Izbeljivanje avitalnog zuba</td>
+					<td>3 000 DIN</td>
+				</tr>
+				<tr>
+					<td>Izbeljivanje zuba trejom (kucna varijanta) po vilici</td>
+					<td>7 000 DIN</td>
+				</tr>
+				<tr>
+					<td>Izbeljivanje zuba u ordinaciji (po vilici)</td>
+					<td>8 000 DIN</td>
+				</tr>
+				<tr>
+					<td>Postavljanje cirkona (zubnog nakita)</td>
+					<td>800 DIN</td>
+				</tr>
+				<tr>
+					<td>Uklanjanje mekih naslaga</td>
+					<td>500 DIN</td>
+				</tr>
+			</tbody>
+		</table>
+		<hr class="my-4">
+	</div>
+
+<?php require ('partials/footer.php'); ?>
