60c1689988b7ebbfca2bd4e50d427ec22d3cbd3c
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-03-21 20:38:32 +0100

    fix up izbeljivanje zuba

--- views/estetska_stomatologija/izbeljivanje_zuba.view.php
+++ views/estetska_stomatologija/izbeljivanje_zuba.view.php
@@ -9,53 +9,44 @@
 			<hr>
 			<div class="col-12">
 				<p class="lead">
-					Estetika u stomatologiji je veoma povezana sa bojom vaših zuba. Beljenjem zuba u našoj ordinaciji dobijate holivudski osmeh kojim se svako ponosi.
+					Estetika u stomatologiji je veoma povezana sa bojom vaših zuba.
+					<br class="d-none d-lg-block">
+					Beljenjem zuba u našoj ordinaciji dobijate <mark style="background-color: #9fdfbf"><em>holivudski osmeh kojim se svako ponosi</em></mark>.
 					<br><br>
-					Pružamo dve vrste beljenja: ambulantno sa laserom i kucnu varijantu sa trejom. Vaše je samo da odaberete, a obe mogucnosti daju fantasticne rezultate.
+					Pružamo dve vrste beljenja:
+					<ol>
+						<li>
+							ambulantno sa laserom
+						</li>
+						<li>
+							kućnu varijantu sa trejom.
+						</li>
+					</ol> 
+					<p class="lead">Vaše je samo da odaberete, a obe mogućnosti daju <u>fantastične rezultate</u>.</p>
 				</p>
 			</div>
 		</div>
-		<hr class="my-3">
 	</div>
 
-	<image-carousel>
-	</image-carousel>
-
 	<!-- Carousel -->
-	<div class="container-sm w-50">
-		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-			</ol>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img src="images/Izbeljivanje/1.jpg" class="d-block w-100 h-100">
-				</div>
-				<div class="carousel-item">
-					<img src="images/Izbeljivanje/2.jpg" class="d-block w-100 h-100">
-				</div>
-				<div class="carousel-item">
-					<img src="images/Izbeljivanje/3.jpg" class="d-block w-100 h-100">
-				</div>
-				<div class="carousel-item">
-					<img src="images/Izbeljivanje/4.jpg" class="d-block w-100 h-100">
-				</div>
+	<div class="container">
+		<div class="row px-2">
+			<div class="col-6 col-sm-6 col-md-3 p-1">
+				<img src="images/Izbeljivanje/1.jpg" class="d-block w-100 h-100">
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+			<div class="col-6 col-sm-6 col-md-3 p-1">
+				<img src="images/Izbeljivanje/2.jpg" class="d-block w-100 h-100">
+			</div>
+			<div class="col-6 col-sm-6 col-md-3 p-1">
+				<img src="images/Izbeljivanje/3.jpg" class="d-block w-100 h-100">
+			</div>
+			<div class="col-6 col-sm-6 col-md-3 p-1">
+				<img src="images/Izbeljivanje/4.jpg" class="d-block w-100 h-100">
+			</div>
 		</div>
 	</div>
 	<div class="container">
-		<hr class="my-4">	
+		<hr class="my-3">	
 	</div>
 
 <?php require ('views/partials/footer.php'); ?>
